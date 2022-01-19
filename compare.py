2ef3c0f20e1ac73bf659fb96d327bc26d89b4806
shawnniuu <j5469455@gmail.com>
2022-01-19 11:56:31 +0800

    Python file

---
+++ compare.py
@@ -0,0 +1,8 @@
+x = 5
+
+print(x == 5) #True
+print(x != 5) #False
+print(x > 2) #True
+print(x < 2) #False
+print(x >= 2) #True
+print(x <= 2) #False
---
+++ if.py
@@ -0,0 +1,5 @@
+rain = input('請問今天有沒有下雨(有/沒有): ')
+if rain == '有':
+	print('在家耍廢')
+if rain == '沒有':
+	print('出門散步')
