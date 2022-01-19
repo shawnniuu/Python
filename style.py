7824f4d930152a64ae7e84adf92a2689eaa3617a
shawnniuu <j5469455@gmail.com>
2022-01-19 11:39:44 +0800

    Python file

---
+++ style.py
@@ -0,0 +1,12 @@
+#好的寫法
+#Good write coding style
+
+x = 'bro'
+print('Hello', x)
+
+My_name = 'shawnmiu'
+My_name_1 = 'shawn'
+age = 19
+height = 170
+weight = 95
+print(My_name, age, height, weight)
