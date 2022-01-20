fe755926c5ad9a2987a948ed3f8c6a3811095906
shawnniuu <j5469455@gmail.com>
2022-01-20 15:27:43 +0800

    Python file

--- 留言分析程式.py
+++ 留言分析程式.py
@@ -4,12 +4,13 @@ data = []
 count = 0
 with open('reviews.txt','r') as f:
 	for line in f:
-		data.append(line.strip())
+		data.append(line)
 		count += 1
-		if count % 10000 ==0:
+		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
 
-print(data[0])
-print('---------------------')
-print(data[1])
+print('檔案讀取完畢,總共有', len(data),'筆資料')
+sum_len = 0 
+for d in data:
+	sum_len+=len(d)
+print('每一筆留言的平均長度為:', int(sum_len/len(data)), '個字')
