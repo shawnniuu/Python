44a6361c6e9fb4df7330e5f54ff22c9076d4ff75
shawnniuu <j5469455@gmail.com>
2022-01-20 12:08:15 +0800

    Python file

--- import-guess-num.py
+++ import-guess-num.py
@@ -1,13 +1,24 @@
 #3-2程式練習 猜數字遊戲
 import random
 
-r = random.randint(1,100)
+start = int(input('請決定隨機數字範圍開始值: '))
+end = int(input('請決定隨機數字範圍結束值: '))
+i = 0
+r = random.randint(start,end)
 while True:
+	i+=1
 	num = int(input('請輸入猜測數字: '))
 	if r > num:
 		print("還要再猜大一點的數字")
+
 	elif r < num:
 		print("還要再猜小一點的數字")
+
 	elif r == num:
 		print('恭喜你! 終於猜對了')
+		print('這是你猜的第',i,'次')
 		break
+
+	print('這是你猜的第',i,'次')
+	
+	print()
