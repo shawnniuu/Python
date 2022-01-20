5d9e907ad6300c7b2c1e82c2b9dacabcdd490ee4
shawnniuu <j5469455@gmail.com>
2022-01-20 11:24:17 +0800

    Python file

--- password try.py
+++ password try.py
@@ -2,14 +2,17 @@
 
 x = 0
 correct = 'a123456'
-print('最多輸出三次密碼')
+print('最多輸入三次密碼')
+print()
 
 while x<3:
 	passw = input("請輸入密碼: ")
 	if passw != correct:
 		x+=1
-		print('密碼錯誤! 還有%d次機會'%(3-x))
+		if x < 3:
+			print('密碼錯誤! 還有%d次機會'%(3-x))
+		else:
+			print('3次密碼錯誤! 冷卻30秒')
 	else:
 		print('登入成功')
 		x=3
-
